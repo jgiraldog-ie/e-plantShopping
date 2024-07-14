7b32c1945f2e517a8b79854e9da3f632adbd2107
Juan Camilo <jgiraldog.ie@gmail.com>
2024-07-14 01:10:44 -0400

    Add Backgorund image

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -273,8 +273,8 @@ function ProductList() {
                         <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
                         <a href="/" style={{textDecoration:'none'}}>
                             <div>
-                                <h3 style={{color:'white'}}>Paradise Nursery</h3>
-                                <i style={{color:'white'}}>Where Green Meets Serenity</i>
+                                <h3 style={{color:'white'}}>Virtual Verde</h3>
+                                <i style={{color:'white'}}>Where Plants find a Home</i>
                             </div>
                         </a>
                     </div>
